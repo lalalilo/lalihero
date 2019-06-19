497a1f983906ba1ae79ca4fc1421093156e46393
lgelin <lucile@lalilo.com>
2019-06-19 15:58:42 +0200

    json with texts for first step of story

--- src/StoryIntroduction.tsx
+++ src/StoryIntroduction.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Page, Content } from "./Layout";
+import {Page1} from "./story/page1"
 
 interface StoryIntroductionProps {
   text: string;
@@ -8,8 +9,8 @@ interface StoryIntroductionProps {
 export const StoryIntroduction: React.FC<StoryIntroductionProps> = props => {
   return (
     <Page>
+      <Page1 />
       <Content>{props.text}</Content>
     </Page>
   );
 };
-
