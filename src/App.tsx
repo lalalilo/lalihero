33370b9ddf7fe8c7fbb4f863be764cb49982071e
stevemajou <steve@lalilo.com>
2019-06-19 11:35:01 +0200

    created homepage file

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-function Index() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HomePage } from "./Home";
 
 function AppRouter() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about/">About</Link>
-            </li>
-            <li>
-              <Link to="/users/">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Route path="/" exact component={Index} />
-        <Route path="/about/" component={About} />
-        <Route path="/users/" component={Users} />
-      </div>
+      <Route path="/" exact component={HomePage} />
     </Router>
   );
 }
